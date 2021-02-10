74774011dc1692474b27ed2c31618e199726814e
Alvaro Carballo <alv.carballo@outlook.es>
2021-02-10 17:25:41 +0100

    Práctica finalizada versión 1.0 Corrección de erratas

--- src/es/studium/Practica5Pyton/Salida.py
+++ src/es/studium/Practica5Pyton/Salida.py
@@ -5,45 +5,49 @@ Created on 5 feb. 2021
 @author: Alvca
 '''
 #Creamos las variables que vamos a usar
+import statistics
 from statistics import mean, mode, variance
-lista = []
-titulo = []
 caracteristicas = []
 caracteristica1 = []
 caracteristica2 = []
 caracteristica3 = []
 caracteristica4 = []
 caracteristica5 = []
-#Preguntamos qque fichero queremos abrir
+#Preguntamos qué fichero queremos abrir
 nombreFichero =input("Indique el nombre del fichero a abrir: ")
 #Controlamos posibles errores
 try:
     #Leemos el fichero y quitamos el \n
     with open (nombreFichero,'r') as archivo:
         lista = list(map(str.rstrip, archivo))
-#indicamos qque ocurrira si hay errores
+#indicamos que ocurrirá si hay errores
 except OSError:
     print('No se puede abrir el archivo', nombreFichero)
-#Si no hay errores seguimos ejecutando el siguiente codigo
+#Si no hay errores seguimos ejecutando el siguiente código
 else:
     archivo.close()
     print('Hemos hacedido al archivo', nombreFichero, 'correctamente')
     #Recorremos la lista y guardamos 2 nuevas listas
     x=0
     for lin in lista:
-        if x==0: #Si es la primera linea guardamos en la lista de titulos
-            #Leemos las lineas de la lista y la metemos en una nueva lista separando el " - "
+        if x==0: #Si es la primera línea guardamos en la lista de títulos
+            #Leemos las líneas de la lista y la metemos en una nueva lista separando el " - "
             titulo = lin.split(" - ")
+            caracteristicas.append(lin.split(" - "))
             x+=1
-        else:#Si no es la primera linea, guardamos en caracteristicax
-            #Leemos las lineas de la lista y la metemos en una nueva lista separando el " - "
-            caracteristicas = lin.split(" - ")
-            #Guardamos cada caracteristica en su lista
-            caracteristica1.append(int(caracteristicas[0]))
-            caracteristica2.append(int(caracteristicas[1]))
-            caracteristica3.append(int(caracteristicas[2]))
-            caracteristica4.append(int(caracteristicas[3]))
-            caracteristica5.append(int(caracteristicas[4]))
+        else:#Si no es la primera línea, guardamos en característica
+            #Leemos las líneas de la lista y la metemos en una nueva lista separando el " - "
+            caracteristica = lin.split(" - ")
+            caracteristicas.append(lin.split(" - "))
+            #Guardamos cada característica  en su lista
+            caracteristica1.append(int(caracteristica[0]))
+            caracteristica2.append(int(caracteristica[1]))
+            caracteristica3.append(int(caracteristica[2]))
+            caracteristica4.append(int(caracteristica[3]))
+            caracteristica5.append(int(caracteristica[4]))
+            
+    
+    
     #Imprimimos el minimo, el maximo, la media, la moda y la varianza
     print("Valores Mínimos:")
     print(titulo[0]+": "+str(min(caracteristica1)))
@@ -65,6 +69,13 @@ else:
     print(titulo[2]+": "+str(mean(caracteristica3)))
     print(titulo[3]+": "+str(mean(caracteristica4)))
     print(titulo[4]+": "+str(mean(caracteristica5)))
+    
+    print("Valores mediana:")
+    print(titulo[0]+": "+str(statistics.median(caracteristica1)))
+    print(titulo[1]+": "+str(statistics.median(caracteristica2)))
+    print(titulo[2]+": "+str(statistics.median(caracteristica3)))
+    print(titulo[3]+": "+str(statistics.median(caracteristica4)))
+    print(titulo[4]+": "+str(statistics.median(caracteristica5)))
     
     print("Valores Moda:")
     print(titulo[0]+": "+str(mode(caracteristica1)))
